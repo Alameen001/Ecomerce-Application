0b9fc04574ec6eeb92c5eeec50cc2793c09094d9
alameen001 <ameenaash36@gmail.com>
2022-07-08 10:58:39 +0530

    first commit

---
+++ lib/presentaion/screens/Home/home_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:shopx/presentaion/screens/Home/widget/Barand.dart';
+import 'package:shopx/presentaion/screens/Home/widget/appbar.dart';
+import 'package:shopx/presentaion/screens/Home/widget/circleAvatae_sliding.dart';
+import 'package:shopx/presentaion/screens/Home/widget/fashinModel.dart';
+import 'package:shopx/presentaion/screens/Home/widget/headline.dart';
+import 'package:shopx/presentaion/screens/Home/widget/poster_img.dart';
+import 'package:shopx/presentaion/screens/Search/search.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        drawer: Drawer(),
+        appBar: PreferredSize(
+          
+          child: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                
+              ],
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+                    Spacer(),
+                    AppbarTitlename(),
+                    Spacer(),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.notifications_active_rounded)),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: ScreenSearch(),
+                ),
+              ],
+            ),
+          ),
+          preferredSize: Size.fromHeight(130),
+        ),
+        body:ListView(
+          children: [
+            CircleSliding(),
+            PosterImg(),
+            BarandImg(),
+            FashionModels(),
+            HeadlineBrand(),
+          ],
+        )  ,
+      ),
+    );
+  }
+}
